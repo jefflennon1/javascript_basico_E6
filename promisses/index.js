105678654b9cb0cffe28a68a988149b0c27891b6
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-21 19:12:37 -0300

    inicio. Ainda aprendendo direito sobre o conceito de promisses

---
+++ promisses/index.js
@@ -0,0 +1,26 @@
+var minhaPromisse = ()=>{
+  return new Promise((resolve, reject)=>{
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'https://api.github.com/users/jefflennon1');
+      xhr.send(null);
+
+      xhr.onreadystatechange = ()=>{
+        if(xhr.readyState === 4){
+          if(xhr.status ===200){
+            resolve(JSON.parse(xhr.responseText))
+          }else{
+            reject('ERRO');
+          }
+        }
+      }
+  })
+}
+
+minhaPromisse()
+  .then((response)=>{
+    console.log(response);
+  })
+  .catch((error)=>{
+    console.warn(error)
+  })
+
