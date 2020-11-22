cf76425361e7e8c19e039c706af1cc108a879891
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-22 16:16:35 -0300

    Classes e herança no JS

--- ES6/index.js
+++ ES6/index.js
@@ -1,9 +1,21 @@
-class teste{
-   oi(){
-
+class List {
+  constructor(){
+    this.data = [];
   }
 
-  outro(){
-    
+  add(data){
+    this.data.push(data);
+    console.log(this.data)
   }
+}
+
+class TodoList extends List{
+ 
+}
+
+const minhaLista = new TodoList();
+
+function addNewTodo(){
+  minhaLista.add('novo');
+  
 }
