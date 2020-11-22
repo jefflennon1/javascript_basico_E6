f27f72a40c378b483cb0c098fd7cf5c9a2f4178b
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-22 17:42:28 -0300

    Percorrendo arrays com map, reduce, filter e find

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,24 +1,19 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Matematica = /*#__PURE__*/function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 8));
+var numbers = [1, 2, 3, 5, 6, 9];
+var exibeNumbers = numbers.map(function (item, index) {
+  return item * index;
+});
+console.log(numbers);
+var somaNumebrs = numbers.reduce(function (total, proximo) {
+  return total + proximo;
+});
+console.log(somaNumebrs);
+var filterNumbers = numbers.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filterNumbers);
+var findNumbers = numbers.find(function (item) {
+  return item === 2;
+});
+console.log(findNumbers);
--- ES6/index.js
+++ ES6/index.js
@@ -1,7 +1,22 @@
-class Matematica{
-  static soma(a , b){
-    return a + b;
-  }
-}
+const numbers = [1,2,3,5,6,9];
 
-console.log(Matematica.soma(1,8));
+const exibeNumbers = numbers.map((item, index)=>{
+  return item * index;
+})
+
+console.log(numbers);
+
+const somaNumebrs = numbers.reduce((total, proximo)=>{
+  return total + proximo;
+})
+console.log(somaNumebrs);
+
+const filterNumbers = numbers.filter((item)=>{
+  return item %2 === 0;
+})
+console.log(filterNumbers);
+
+const findNumbers = numbers.find((item)=>{
+  return item === 2;
+})
+console.log(findNumbers);
