de8e10d8cf99772882aaf982f2eee67c5661c116
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-22 17:58:49 -0300

    Diminuindo a verbosidade das minhas funções

--- ES6/index.js
+++ ES6/index.js
@@ -1,22 +1,13 @@
 const numbers = [1,2,3,5,6,9];
 
-const exibeNumbers = numbers.map((item, index)=>{
-  return item * index;
-})
-
+const exibeNumbers = numbers.map((item, index) => item * index);
 console.log(numbers);
 
-const somaNumebrs = numbers.reduce((total, proximo)=>{
-  return total + proximo;
-})
+const somaNumebrs = numbers.reduce((total, proximo)=>total + proximo);
 console.log(somaNumebrs);
 
-const filterNumbers = numbers.filter((item)=>{
-  return item %2 === 0;
-})
+const filterNumbers = numbers.filter(item => item % 2 === 0);
 console.log(filterNumbers);
 
-const findNumbers = numbers.find((item)=>{
-  return item === 2;
-})
+const findNumbers = numbers.find(item => item === 2);
 console.log(findNumbers);
