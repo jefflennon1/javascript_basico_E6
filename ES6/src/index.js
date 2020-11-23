82dea21880fa85c989d900ac104bf012deb90e79
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-23 17:55:29 -0300

    axios+class+try/catch+async/await+static_method

--- ES6/src/index.js
+++ ES6/src/index.js
@@ -1,19 +1,31 @@
-const MinhaPromise = ()=> new Promise((resolve, reject)=>{
-   setTimeout(()=>{resolve('ok')}, 2000);
-})
-
-
-// MinhaPromise().then((response)=>{
-//   console.log(response);
-// }).catch((error)=>{
-//   console.log(error);
+// const MinhaPromise = ()=> new Promise((resolve, reject)=>{
+//    setTimeout(()=>{resolve('ok')}, 2000);
 // })
+// // MinhaPromise().then((response)=>{
+// //   console.log(response);
+// // }).catch((error)=>{
+// //   console.log(error);
+// // })
 
-async function executaPromisse(){
-  console.log( await MinhaPromise());
-  console.log( await MinhaPromise());
-  console.log( await MinhaPromise());
+// async function executaPromisse(){
+//   console.log( await MinhaPromise());
+//   console.log( await MinhaPromise());
+//   console.log( await MinhaPromise());
+// }
+// executaPromisse();
+
+import axios from 'axios';
+
+class Api{
+  static async getUserInfo(username){
+     try{
+          const response = await axios.get(`https://api.github.com/users/${username}`);
+         console.log(response)
+      } catch (err){
+          console.warn('User not found!');
+      }
+  }
 }
 
+Api.getUserInfo('jefflennon15454545');
 
-executaPromisse();
