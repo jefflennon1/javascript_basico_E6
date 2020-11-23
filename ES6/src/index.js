3d41f435643bdd27944d628c32386505b7794cf0
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-23 13:39:34 -0300

    async/await

--- ES6/src/index.js
+++ ES6/src/index.js
@@ -1,1 +1,19 @@
-alert('teste')
+const MinhaPromise = ()=> new Promise((resolve, reject)=>{
+   setTimeout(()=>{resolve('ok')}, 2000);
+})
+
+
+// MinhaPromise().then((response)=>{
+//   console.log(response);
+// }).catch((error)=>{
+//   console.log(error);
+// })
+
+async function executaPromisse(){
+  console.log( await MinhaPromise());
+  console.log( await MinhaPromise());
+  console.log( await MinhaPromise());
+}
+
+
+executaPromisse();
