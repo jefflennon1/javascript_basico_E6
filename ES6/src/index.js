e4de3ea0b9bc93a93868fbfcf772881ea70ac92d
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-24 09:14:17 -0300

    Rendenrizando na tela

--- ES6/src/index.js
+++ ES6/src/index.js
@@ -2,6 +2,7 @@ class App{
   constructor(){
     this.repositories = [];
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
     this.registerHandles();
   }
   registerHandles(){
@@ -15,8 +16,36 @@ class App{
           avatar_url: 'https://avatars2.githubusercontent.com/u/53300744?v=4',
           html_url: 'https://api.github.com/users/jefflennon1'
         });
-        console.log(this.repositories);
-        
+      
+      this.render();  
+    }
+
+    render(){
+      this.listEl.innerHTML = '';
+
+      this.repositories.forEach(repo => {
+          let imgEl = document.createElement('img')
+          imgEl.setAttribute('src', repo.avatar_url);
+
+          let titleEl = document.createElement('strong');
+          titleEl.appendChild(document.createTextNode(repo.name));
+
+          let descriptionEl = document.createElement('p');
+          descriptionEl.appendChild(document.createTextNode(repo.description));
+
+          let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+          let listNewEl = document.createElement('li');
+          listNewEl.appendChild(imgEl);
+          listNewEl.appendChild(titleEl);
+          listNewEl.appendChild(descriptionEl);
+          listNewEl.appendChild(linkEl);
+
+          this.listEl.appendChild(listNewEl);
+      });
     }
 }
 
