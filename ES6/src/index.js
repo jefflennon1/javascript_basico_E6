e4ceceb1f527af96f42849e690df5038226f28f4
jefferson lennon <jeffersonlennon1@outlook.com>
2020-11-24 08:48:40 -0300

    Começando a criar uma aplicaçao bem simples que põe em prática os conceitos vistos até agora

--- ES6/src/index.js
+++ ES6/src/index.js
@@ -1,24 +1,26 @@
 class App{
-    constructor(){
-      this.repositories = [];
-      this.formEl = document.getElementById('repo-form');
-
-      this.registerHandles();
+  constructor(){
+    this.repositories = [];
+    this.formEl = document.getElementById('repo-form');
+    this.registerHandles();
+  }
+  registerHandles(){
+       this.formEl.onsubmit = event => this.addRepository(event);
     }
-    registerHandles(){
-      this.formEl.onsubmit = event => this.addrepository(event);
-    }
-    addrepository(event){
+    addRepository(event){
       event.preventDefault();
-
-      this.repositories.push({
-        name: 'jefflennon1',
-        description: 'Qualquercoisaa',
-        avatar_url: 'https://avatars2.githubusercontent.com/u/53300744?v=4',
-        html_url:'https://github.com/jefflennon1',
-      });
-      console.log(this.repositories);
+        this.repositories.push({
+          name: 'jefflennon1',
+          description: 'qualquer coisa',
+          avatar_url: 'https://avatars2.githubusercontent.com/u/53300744?v=4',
+          html_url: 'https://api.github.com/users/jefflennon1'
+        });
+        console.log(this.repositories);
+        
     }
 }
 
 new App();
+
+
+
